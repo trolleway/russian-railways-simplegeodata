06553dd9dcb2e988f402f656d024e16257f49416
Artem Svetlov <trolleway@yandex.ru>
2019-10-13 23:21:53 +0300

    map generation in png

---
+++ rendermap/render_map.py
@@ -0,0 +1,3 @@
+import os
+os.system('''c:\NextGIS\bin\ngqgis.exe --snapshot ..\map.png   ..\russian-railways-simplegeodata.qgs''')
+#os.system('ngqgis --snapshot ../map.png   ../russian-railways-simplegeodata.qgs')
