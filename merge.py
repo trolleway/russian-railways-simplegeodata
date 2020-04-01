4b5c99802696f873413ba21fe895488489916d47
trolleway <trolleway@yandex.ru>
2020-04-01 11:03:56 +0300

    update

--- merge.py
+++ merge.py
@@ -1,9 +1,12 @@
 import os
 
-os.unlink('mainlines.gpkg')
-os.unlink('secondary.gpkg')
+try:
+    os.unlink('mainlines.gpkg')
+    os.unlink('secondary.gpkg')
+except:
+    pass
 
 os.system('ogrmerge  -f GPKG -o mainlines.gpkg -single -overwrite_ds rzd\mainlines\*.geojson')
 os.system('ogrmerge  -f GPKG -o secondary.gpkg -single -overwrite_ds rzd\secondary\*.geojson')
-os.system('ogrmerge.py  -f GPKG -o mainlines.gpkg -single -overwrite_ds rzd\mainlines\*.geojson')
-os.system('ogrmerge.py  -f GPKG -o secondary.gpkg -single -overwrite_ds rzd\secondary\*.geojson')
+os.system('ogrmerge.py  -f GPKG -o mainlines.gpkg -single -overwrite_ds ' + os.path.join('rzd','mainlines','*.geojson'))
+os.system('ogrmerge.py  -f GPKG -o secondary.gpkg -single -overwrite_ds '+ os.path.join('rzd','secondary','*.geojson'))
